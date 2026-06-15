0561eddb25c6b983376064f3d48cdfc4e0360748
Mathias <mfred@tuta.com>
2026-06-15 20:54:54 +0200

    Refactor packet parsing with struct.Struct and NamedTuple

--- server/packet.py
+++ server/packet.py
@@ -1,87 +1,85 @@
 import socket
-import struct
+from enum import IntEnum
+from typing import NamedTuple
+
+from protocol import (
+    PKT_EXPECTED_LEN,
+    PKT_FMT,
+    PKT_PORT_MAX,
+    PKT_PORT_MIN,
+)
+
+
+class EtherType(IntEnum):
+    IPV4 = 0x0800
+
+
+class IpProto(IntEnum):
+    UDP = 17
+
+
+class IpOffset:
+    PROTO = 9
+    SRC_START = 12
+    SRC_END = 16
+    IHL_MASK = 0x0F
+    IHL_WORD = 4
+    MIN_HDR_LEN = 20
+
 
 ETH_HDR_LEN = 14
-ETHERTYPE_IPV4 = b"\x08\x00"
-
-IP_PROTO_OFFSET = 9
-IP_SRC_OFFSET = 12
-IP_SRC_END = 16
-IP_IHL_MASK = 0x0F
-IP_IHL_WORD = 4
-IP_PROTO_UDP = 17
-MIN_IP_HDR_LEN = 20
-
 UDP_HDR_LEN = 8
 
 ADDR_PKT_TYPE_IDX = 2
 PACKET_OUTGOING = 4
 ADDR_MIN_LEN = 3
 
-PKT_PORT_SIZE = 2
-PKT_TTL_SIZE = 2
-PKT_TIMESTAMP_SIZE = 8
-PKT_NONCE_SIZE = 16
-PKT_SIG_SIZE = 64
 
-PKT_PORT_START = 0
-PKT_PORT_END = PKT_PORT_START + PKT_PORT_SIZE
-
-PKT_TTL_START = PKT_PORT_END
-PKT_TTL_END = PKT_TTL_START + PKT_TTL_SIZE
-
-PKT_TS_START = PKT_TTL_END
-PKT_TS_END = PKT_TS_START + PKT_TIMESTAMP_SIZE
-
-PKT_NONCE_START = PKT_TS_END
-PKT_NONCE_END = PKT_NONCE_START + PKT_NONCE_SIZE
-
-PKT_SIG_START = PKT_NONCE_END
-PKT_SIG_END = PKT_SIG_START + PKT_SIG_SIZE
-
-PKT_EXPECTED_LEN = PKT_SIG_END
-PKT_PORT_MIN = 1
-PKT_PORT_MAX = 65535
+class Packet(NamedTuple):
+    port: int
+    ttl: int
+    timestamp: int
+    nonce: bytes
+    signature: bytes
 
 
 def parse(payload):
     if len(payload) != PKT_EXPECTED_LEN:
         return None
 
-    port = struct.unpack("!H", payload[PKT_PORT_START:PKT_PORT_END])[0]
-    ttl = struct.unpack("!H", payload[PKT_TTL_START:PKT_TTL_END])[0]
-    timestamp = struct.unpack("!Q", payload[PKT_TS_START:PKT_TS_END])[0]
-    nonce = payload[PKT_NONCE_START:PKT_NONCE_END]
+    packet = Packet._make(PKT_FMT.unpack(payload))
 
-    if port < PKT_PORT_MIN or port > PKT_PORT_MAX:
+    if not (PKT_PORT_MIN <= packet.port <= PKT_PORT_MAX):
         return None
 
-    if ttl == 0:
+    if packet.ttl == 0:
         return None
 
-    return port, ttl, timestamp, nonce, payload[PKT_SIG_START:PKT_SIG_END]
+    return packet
 
 
 def validate_frame(frame, addr):
     if len(addr) >= ADDR_MIN_LEN and addr[ADDR_PKT_TYPE_IDX] == PACKET_OUTGOING:
         return None
 
-    if len(frame) < ETH_HDR_LEN + MIN_IP_HDR_LEN + UDP_HDR_LEN:
+    if len(frame) < ETH_HDR_LEN + IpOffset.MIN_HDR_LEN + UDP_HDR_LEN:
         return None
 
-    if frame[12:14] != ETHERTYPE_IPV4:
+    if frame[12:14] != EtherType.IPV4.to_bytes(2, "big"):
         return None
 
     ip_start = ETH_HDR_LEN
-    if frame[ip_start + IP_PROTO_OFFSET] != IP_PROTO_UDP:
+    if frame[ip_start + IpOffset.PROTO] != IpProto.UDP:
         return None
 
-    ip_hl = (frame[ip_start] & IP_IHL_MASK) * IP_IHL_WORD
+    ip_hl = (frame[ip_start] & IpOffset.IHL_MASK) * IpOffset.IHL_WORD
 
     if len(frame) < ip_start + ip_hl + UDP_HDR_LEN:
         return None
 
-    src_ip = socket.inet_ntoa(frame[ip_start + IP_SRC_OFFSET : ip_start + IP_SRC_END])
+    src_ip = socket.inet_ntoa(
+        frame[ip_start + IpOffset.SRC_START : ip_start + IpOffset.SRC_END]
+    )
     payload = frame[ip_start + ip_hl + UDP_HDR_LEN :]
 
     return src_ip, payload
