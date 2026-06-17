1d1c78cb8d276b5977bd2254fdfd27114f133729
Mathias <mfred@tuta.com>
2026-06-17 21:48:56 +0200

    Accept Path for database location

--- server/database.py
+++ server/database.py
@@ -1,8 +1,9 @@
 import sqlite3
+from pathlib import Path
 
 
 class Database:
-    def __init__(self, db_path="portkey.db"):
+    def __init__(self, db_path=Path("portkey.db")):
         self.conn = sqlite3.connect(db_path)
 
     def execute(self, sql, params=()):
